0f618db1703389b423de72b46322ed346e1acf8b
Ted M. Young <tedyoung@gmail.com>
2024-01-24 15:32:34 -0800

    LAB #3: Thought about how to expose the Player and Dealer hands in a way that doesn't break encapsulation (too much). Ideally, we'd use a Value Object and not return Hand.

--- src/main/java/com/jitterted/ebp/blackjack/domain/Game.java
+++ src/main/java/com/jitterted/ebp/blackjack/domain/Game.java
@@ -55,27 +55,43 @@ public class Game {
     public void displayGameState() {
         System.out.print(ansi().eraseScreen().cursor(1, 1));
         System.out.println("Dealer has: ");
-        System.out.println(ConsoleHand.displayFaceUpCard(dealerHand));
+        System.out.println(ConsoleHand.displayFaceUpCard(dealerHand()));
 
         // second card is the hole card, which is hidden, or "face down"
         ConsoleCard.displayBackOfCard();
 
         System.out.println();
         System.out.println("Player has: ");
-        System.out.println(ConsoleHand.cardsAsString(playerHand));
-        System.out.println(" (" + playerHand.value() + ")");
+        System.out.println(ConsoleHand.cardsAsString(playerHand()));
+        System.out.println(" (" + playerHand().value() + ")");
+    }
+
+    // 1. Return Hand instance field - breaks encapsulation: allows callers to modify internals of Game
+    //    (Violates Game's integrity) - also not a Snapshot (point in time view)
+    // 2. List<Card> - Stream<Card>: what about faceUpCard(), and value()
+    //    2a. Could have separate Query methods for the other aspects
+    // 3. HandDto - just has "primitive" data (Strings & numbers), however DTOs live in Adapters
+    //==> 4. HandValue, HandInfo, HandView
+    //       - Value Object with Rich Domain Types: Cards, Face Up Card, int
+    // 5. HandSnapshot - contains internal data from Hand, used exclusively for persistence
+    public Hand dealerHand() {
+        return dealerHand;
+    }
+
+    public Hand playerHand() {
+        return playerHand;
     }
 
     public void displayFinalGameState() {
         System.out.print(ansi().eraseScreen().cursor(1, 1));
         System.out.println("Dealer has: ");
-        System.out.println(ConsoleHand.cardsAsString(dealerHand));
-        System.out.println(" (" + dealerHand.value() + ")");
+        System.out.println(ConsoleHand.cardsAsString(dealerHand()));
+        System.out.println(" (" + dealerHand().value() + ")");
 
         System.out.println();
         System.out.println("Player has: ");
-        System.out.println(ConsoleHand.cardsAsString(playerHand));
-        System.out.println(" (" + playerHand.value() + ")");
+        System.out.println(ConsoleHand.cardsAsString(playerHand()));
+        System.out.println(" (" + playerHand().value() + ")");
     }
 
 
