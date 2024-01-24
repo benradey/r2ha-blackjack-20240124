c71b9f8e48d136cc8b05ae36b41b33d36829ae0b
Ted M. Young <tedyoung@gmail.com>
2024-01-24 12:25:55 -0800

    LAB #2: Added Characterization test for cardsAsString()

--- src/test/java/com/jitterted/ebp/blackjack/HandDisplayTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/HandDisplayTest.java
@@ -11,7 +11,18 @@ public class HandDisplayTest {
     public void displayFaceUpCard() throws Exception {
         Hand hand = new Hand(List.of(new Card(Suit.HEARTS, Rank.ACE)));
 
-      assertThat(ConsoleHand.displayFaceUpCard(hand))
+        assertThat(ConsoleHand.displayFaceUpCard(hand))
                 .isEqualTo("[31m┌─────────┐[1B[11D│A        │[1B[11D│         │[1B[11D│    ♥    │[1B[11D│         │[1B[11D│        A│[1B[11D└─────────┘");
+    }
+
+    @Test
+    void cardsAsString() {
+        Hand hand = new Hand(List.of(new Card(Suit.CLUBS, Rank.SIX),
+                                     new Card(Suit.DIAMONDS, Rank.NINE)));
+
+        String stringCards = hand.cardsAsString();
+
+        assertThat(stringCards)
+                .isEqualTo("[30m┌─────────┐[1B[11D│6        │[1B[11D│         │[1B[11D│    ♣    │[1B[11D│         │[1B[11D│        6│[1B[11D└─────────┘[6A[1C[31m┌─────────┐[1B[11D│9        │[1B[11D│         │[1B[11D│    ♦    │[1B[11D│         │[1B[11D│        9│[1B[11D└─────────┘");
     }
 }
