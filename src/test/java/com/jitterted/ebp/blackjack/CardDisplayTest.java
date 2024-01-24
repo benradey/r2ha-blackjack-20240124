c6f32c6222b0c9cc680f4ba7a41fd839f77dabff
Ted M. Young <tedyoung@gmail.com>
2024-01-24 11:20:58 -0800

    LAB #1: Characterization test for display of 10-ranked Card as a String

---
+++ src/test/java/com/jitterted/ebp/blackjack/CardDisplayTest.java
@@ -0,0 +1,18 @@
+package com.jitterted.ebp.blackjack;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class CardDisplayTest {
+
+    @Test
+    void displayTenAsString() {
+        Card card = new Card(Suit.CLUBS, Rank.TEN);
+
+        String displayString = card.display();
+
+        assertThat(displayString)
+                .isEqualTo("[30m┌─────────┐[1B[11D│10       │[1B[11D│         │[1B[11D│    ♣    │[1B[11D│         │[1B[11D│       10│[1B[11D└─────────┘");
+    }
+}
