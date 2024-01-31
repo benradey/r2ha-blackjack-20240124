cb6ed822aa1eeaa066a77a97ffaf8a388f6ee32e
Ted M. Young <tedyoung@gmail.com>
2024-01-31 11:41:17 -0800

    TDD: GET to /game returns correct view (template) name

--- src/test/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackControllerTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackControllerTest.java
@@ -1,8 +1,15 @@
 package com.jitterted.ebp.blackjack.adapter.in.web;
 
+import com.jitterted.ebp.blackjack.domain.Card;
+import com.jitterted.ebp.blackjack.domain.Deck;
 import com.jitterted.ebp.blackjack.domain.Game;
+import com.jitterted.ebp.blackjack.domain.Rank;
 import com.jitterted.ebp.blackjack.domain.ShuffledDeck;
+import com.jitterted.ebp.blackjack.domain.StubDeck;
+import com.jitterted.ebp.blackjack.domain.Suit;
 import org.junit.jupiter.api.Test;
+
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -21,5 +28,22 @@ class BlackjackControllerTest {
                 .as("Expected 2 cards to be dealt to the player from the Initial Deal")
                 .hasSize(2);
     }
+
+    @Test
+    void gameViewPopulatesViewModelWithAllCards() {
+        Deck stubDeck = new StubDeck(List.of(new Card(Suit.DIAMONDS, Rank.TEN),
+                                             new Card(Suit.HEARTS, Rank.TWO),
+                                             new Card(Suit.DIAMONDS, Rank.KING),
+                                             new Card(Suit.CLUBS, Rank.THREE)));
+        Game game = new Game(stubDeck);
+        BlackjackController blackjackController = new BlackjackController(game);
+        blackjackController.startGame();
+
+        String viewName = blackjackController.gameView();
+
+        assertThat(viewName)
+                .isEqualTo("blackjack");
+    }
+
 
 }
