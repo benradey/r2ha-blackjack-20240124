dfd63b5e8df94d2038e70eda936821c70a6439e9
Ted M. Young <tedyoung@gmail.com>
2024-01-31 09:59:52 -0800

    LAB #5: Added test for Blackjack outcome, currently fails as Predicted

--- src/test/java/com/jitterted/ebp/blackjack/domain/GameOutcomeTest.java
+++ src/test/java/com/jitterted/ebp/blackjack/domain/GameOutcomeTest.java
@@ -38,6 +38,17 @@ class GameOutcomeTest {
                 .isEqualTo(GameOutcome.PLAYER_PUSHES_DEALER);
     }
 
+    @Test
+    void playerDealtBlackjackUponInitialDealAndDealerNotDealtBlackjackThenPlayerWinsBlackjack() {
+        Game game = new Game(new StubDeck(Rank.JACK, Rank.NINE,
+                                          Rank.ACE, Rank.TEN));
+
+        game.initialDeal();
+
+        assertThat(game.determineOutcome())
+                .isEqualByComparingTo(GameOutcome.PLAYER_WINS_BLACKJACK);
+    }
+
     private static Game createGameAndDoInitialDeal(Deck deck) {
         Game game = new Game(deck);
         game.initialDeal();
