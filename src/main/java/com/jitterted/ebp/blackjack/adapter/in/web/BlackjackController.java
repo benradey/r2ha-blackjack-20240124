d2bffa1e028c0074b22ac940609ffcf747b1e93f
Ted M. Young <tedyoung@gmail.com>
2024-01-31 11:14:36 -0800

    TDD: [I/O-Free testing] startGame does not (yet) do the initial deal, but we added a reference to our application so the Adapter can reference it

--- src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackController.java
+++ src/main/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackController.java
@@ -1,10 +1,17 @@
 package com.jitterted.ebp.blackjack.adapter.in.web;
 
+import com.jitterted.ebp.blackjack.domain.Game;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BlackjackController {
+
+    private final Game game;
+
+    public BlackjackController(Game game) {
+        this.game = game;
+    }
 
     @PostMapping("/start-game")
     public String startGame() {
---
+++ src/test/java/com/jitterted/ebp/blackjack/adapter/in/web/BlackjackControllerTest.java
@@ -0,0 +1,23 @@
+package com.jitterted.ebp.blackjack.adapter.in.web;
+
+import com.jitterted.ebp.blackjack.domain.Game;
+import com.jitterted.ebp.blackjack.domain.ShuffledDeck;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+class BlackjackControllerTest {
+
+    @Test
+    void startGameResultsInTwoCardsDealtToPlayer() {
+        Game game = new Game(new ShuffledDeck());
+        BlackjackController blackjackController = new BlackjackController(game);
+
+        blackjackController.startGame();
+
+        assertThat(game.playerHand().cards())
+                .as("Expected 2 cards to be dealt to the player from the Initial Deal")
+                .hasSize(2);
+    }
+
+}
